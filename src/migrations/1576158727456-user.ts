7ee3919b79cd98ff734e5bc09cc59e21494a1012
Your Name <you@example.com>
2019-12-12 22:33:12 +0800

    hw

---
+++ src/migrations/1576158727456-user.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class user1576158727456 implements MigrationInterface {
+    name = 'user1576158727456'
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB", undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("DROP TABLE `user`", undefined);
+    }
+
+}
